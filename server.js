53e1ddfda4fbef86b745d109678254e701e7bf38
mvisintin <mvisintin@travelrepublic.co.uk>
2015-05-28 16:13:58 +0100

    Logging | CATCH EVERY 504

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ var server = prerender({
 
 // server.use(prerender.basicAuth());
 // server.use(prerender.whitelist());
-server.use(prerender.blacklist());
+// server.use(prerender.blacklist());
 // server.use(prerender.logger());
 server.use(prerender.removeScriptTags());
-server.use(prerender.httpHeaders());
+// server.use(prerender.httpHeaders());
 // server.use(prerender.inMemoryHtmlCache());
 
 server.use(prerender.rootRequest());
